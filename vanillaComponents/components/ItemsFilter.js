f373c4ccb663ccf4db7b6a944792b5e2046c6289
dongyeopca <ckehdduq95@gmail.com>
2023-03-15 21:23:29 +0900

    feat: 컴포넌트 만들기 연습

---
+++ vanillaComponents/components/ItemsFilter.js
@@ -0,0 +1,17 @@
+import Component from '../core/Component.js';
+
+export default class ItemFilter extends Component {
+  template() {
+    return `
+            <button class='filterBtn' data-is-filter="0">전체보기</button>
+            <button class='filterBtn' data-is-filter="1">활성화 보기</button>
+            <button class='filterBtn' data-is-filter="2">비활성화 보기</button>
+        `;
+  }
+  setEvent() {
+    const { filterItem } = this.$props;
+    this.addEvent('click', '.filterBtn', ({ target }) => {
+      filterItem(Number(target.dataset.isFilter));
+    });
+  }
+}
---
+++ vanillaComponents/index.js
@@ -0,0 +1,8 @@
+import App from './App.js';
+
+new App(document.querySelector('#app'));
+
+// function thischecker(hello) {
+//   console.log(this);
+// }
+// thischecker();
